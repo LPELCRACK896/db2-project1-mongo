7a623658b88dd045643601ea721767c4eed64b34
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-02-13 13:48:36 -0600

    Rreforomar html bookform

--- front/src/Bookform.js
+++ front/src/Bookform.js
@@ -1,58 +1,54 @@
 import './App.css'
 
 function Form(){
-    const categories = ["Aventura", "Ciencia ficcion", "Fantasia", "Gotica", "Novela negra", "Romance", "Biografia", "Distopia"]
-    return(
-        <>
-        <div className="Entra">
-            <p>Title</p>
-            <input></input>
-        </div>
-
-<div className="Entra">
-            <p>Author</p>
-            <input></input>
-</div>
-<div className="Entra">
-            <p>Pages</p>
-            <input></input>
-</div>
-<div className="Entra">
-            <p>Year</p>
-            <input></input>
-</div>
-<div className="Entra">
-            <p>isbn</p>
-            <input></input>
-</div>
-<div className="Entra">
-            <p>Rate</p>
-            <input></input>
-</div>
-<div className="Entra">
-            <p>Description</p>
-            <input></input>
-</div>
-<div className="Entra">
-            <p>Category</p>
-            <select>
-            {categories&&
-              categories.map(category => (
-                <option value={category}>{category}</option>
-              ))
-
-            }
-          </select>
-</div>
-        <div className="Entra">
-            <p>Reviewer rate</p>
-            <input></input>
-        </div>
-
-        <button className='Adicion'>Add book</button>
+  const categories = ["Aventura", "Ciencia ficcion", "Fantasia", "Gotica", "Novela negra", "Romance", "Biografia", "Distopia"]
+  return(
+    <>
+      <div className="Entra">
+        <p>Title</p>
+        <input></input>
+      </div>
+      <div className="Entra">
+        <p>Author</p>
+        <input></input>
+      </div>
+      <div className="Entra">
+        <p>Pages</p>
+        <input></input>
+      </div>
+      <div className="Entra">
+        <p>Year</p>
+        <input></input>
+      </div>
+      <div className="Entra">
+        <p>isbn</p>
+        <input></input>
+      </div>
+      <div className="Entra">
+        <p>Rate</p>
+        <input></input>
+      </div>
+      <div className="Entra">
+        <p>Description</p>
+        <input></input>
+      </div>
+      <div className="Entra">
+        <p>Category</p>
+        <select>
+          {categories&&
+          categories.map(category => (
+            <option value={category}>{category}</option>
+          ))}
+        </select>
+      </div>
+      <div className="Entra">
+        <p>Reviewer rate</p>
+        <input></input>
+      </div>
+      <button className='Adicion'>Add book</button>
         
-        </>
-    )
+    </>
+  )
 }
 
 export default Form
